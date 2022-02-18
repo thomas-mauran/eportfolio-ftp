4019c8b47437deebe7133c8adeb2d154b99d2514
Thomas Mauran <78204354+thomas-mauran@users.noreply.github.com>
2022-02-18 11:00:31 +0100

    official test
    footer update

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <footer>
     <p>Thomas Mauran ©</p>
-    <p>Site entièrement réalisé à la main et hébergé sur mon Raspberry Pi ! </p>
+    <p>Site entièrement réalisé à la main et hébergé dans un conteneur docker sur mon Raspberry Pi ! </p>
 </footer>
